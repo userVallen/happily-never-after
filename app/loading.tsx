aafcd3bb9bcdc8bea656b61708a7fc902a40a5c6
userVallen <vallen180303@gmail.com>
2026-08-02 12:50:31 +0900

    fix: fix loading animation issue on mobile

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,9 +2,9 @@ import LoadingAnimation from '@/components/loading-animation';
 
 export default function Loading() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen">
+    <main className="flex flex-col items-center justify-center min-h-dvh">
       <LoadingAnimation />
-      <p className="text-heading text-xl">Loading...</p>
+      <p className="font-heading text-xl">Loading...</p>
     </main>
   );
 }
